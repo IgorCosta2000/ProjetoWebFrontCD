6733f22b294a17d3bc5ddbafe131a2e8618db7a6
IgorCosta2000 <igorcosazevedo@gmail.com>
2021-09-14 15:08:34 -0300

    componente do filter sem a lógica do botão

--- web/src/View/inicio/index.js
+++ web/src/View/inicio/index.js
@@ -2,12 +2,18 @@ import React from 'react';
 import * as Styled from './styles';
 import Header from '../../components/header';
 import Footer from '../../components/footer';
+import Filter from '../../components/filter';
 function Home() {
   return (
-    <Styled.Container>
+ <Styled.Container>
       <Header />
+        <Styled.ContainerFilter>
+          <Filter title="Todos"/>
+          <Filter title="Hoje"/>
+          <Filter title="Semana"/>
+          <Filter title="Mês"/>
+        </Styled.ContainerFilter>
       <Footer />
-
     </Styled.Container>
   )
 }
